c3ac2e9c43086f7982c9ab7bd1702f4d427494a9
ravirao07 <149283218+ravirao07@users.noreply.github.com>
2024-03-24 03:11:52 +0000

    updateFirst

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React<h1>Ravi Rao</h1>
+          <h1>Ravi Rao</h1>
         </a>
       </header>
     </div>
